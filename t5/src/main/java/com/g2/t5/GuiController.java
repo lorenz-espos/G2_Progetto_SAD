49954558826288ff8e4319a9df01fb081cff13ba
Michele <m.defazio@studenti.unina.it>
2023-05-09 11:30:33 +0200

    variabili

--- t5/src/main/java/com/g2/t5/GuiController.java
+++ t5/src/main/java/com/g2/t5/GuiController.java
@@ -1,14 +1,17 @@
 package com.g2.t5;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GuiController {
     @GetMapping("/main")
-    public String GUIController() {
+    public String GUIController(Model model) {
+        String nome = "Arctic Network";
 
+        model.addAttribute("nome", nome);
         return "main";
     }
-    
+
 }
